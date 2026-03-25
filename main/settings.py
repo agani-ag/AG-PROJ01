ce737592b0ca8f5d70c62ad44fcd41597d356ac0
Ganesh Saravanan <ganesh.s@ciindia.com>
2026-03-25 15:49:37 +0530

    P1B2C01 → Removed REST
    P1B2C01 → Removed REST

--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'objectbank',
 ]
 
@@ -54,17 +53,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'main.urls'
-
-# DRF Configuration
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ],
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-    ],
-}
 
 TEMPLATES = [
     {
@@ -90,7 +78,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'maincore.sqlite3',
+        'NAME': BASE_DIR / 'ag-proj01.sqlite3',
     }
 }
 
--- objectbank/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ObjectbankConfig(AppConfig):
-    name = 'objectbank'
